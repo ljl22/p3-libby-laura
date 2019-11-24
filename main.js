412962f068b9163b8f1628e2df01b3aa9ec19d90
ljl22 <lauraj.libby@gmail.com>
2019-11-24 15:56:17 -0500

    Update layer 1 in html to regions in Ireland, instead of colors

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 //make something flash the the number of times that the name of the color is long
 //global variables
 const myBoxes = document.getElementsByClassName('block');
+const myBoxesLayerB = document.getElementsByClassName('block-b');
 
 // our function - local scope
 function loopThrough() {
 
-  const changeColor = () => this.classList.toggle('lime');
-
-  for(let i = 0; i < this.textContent.length * 2; i++) {
-    setTimeout(changeColor, i * 1000);
+  const changeColor = () => {
+    this.classList.toggle('lime');
   };
+
+  for ( let i = 0; i < this.textContent.length * 2; i++) {
+    setTimeout(changeColor, i * 1000);
+  }
 
 
 };
