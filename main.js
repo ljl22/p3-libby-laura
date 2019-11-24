d023c49eb516b5650b328d43ebad27f4a374601a
ljl22 <lauraj.libby@gmail.com>
2019-11-24 09:55:11 -0500

    Add initial Javascript to flash the number of times a color is long

---
+++ main.js
@@ -0,0 +1,14 @@
+//make something flash the the number of times that the name of the color is long
+//global variables
+const myBoxes = document.getElementsByClassName('color');
+
+// our function
+function loopThrough() {
+
+};
+
+
+// event listener
+for (let i = 0; i < myBoxes.length; i++) {
+  myBoxes[i].addEventListener('click', loopThrough);
+};
