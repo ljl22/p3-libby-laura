bcc3c82a8a56f08c5f472bd6990c764d3b8f7b10
ljl22 <lauraj.libby@gmail.com>
2019-11-25 19:38:42 -0500

    Update flashing number functionality in  main.js

--- src/main.js
+++ src/main.js
@@ -5,12 +5,10 @@ const layerA =  document.getElementById('step-a');
 
 const myBoxesLayerB = document.getElementsByClassName('blockb'); //I think there's an issue pulling here
 const layerB =  document.getElementById('step-b');
-const myNumber = myBoxesLayerB.innerHTML; //not working correctly
 
 
 // our function - local scope
 function loopThrough() {
-
   const changeColor = () => {
     this.classList.toggle('lime');
   };
@@ -20,11 +18,11 @@ function loopThrough() {
 };
 
 
-//loop through for number string***********this is where one of the issues is*****************
+//loop through for number string************************
 function loopThroughNumber() {
-  const changeColorTwo = () => myBoxesLayerB.classList.toggle('lime')
+  const changeColorTwo = () => this.classList.toggle('forest')
 
-  for (let i = 0; i < Number(myNumber) * 2; i++) {
+  for (let i = 0; i < Number(this.textContent) * 2; i++) {
     setTimeout(changeColorTwo, i * 1000);
   }
 }
@@ -39,18 +37,14 @@ function changeLayer () {
  setTimeout(swap, this.textContent.length * 2 * 1000);
 };
 
-//function for number string***************************************
-//function changeLayerNumber () {
-//  const swap = () => {
-//    layerA.classList.add('hide');
-//    layerB.classList.remove('hide');
-//  }
-// setTimeout(swap, this.textContent.length * 2 * 1000);
-//};
 
-
-// event listener
+// event listener for box change
 for (let i = 0; i < myBoxes.length; i++) {
   myBoxes[i].addEventListener('click', loopThrough);
   myBoxes[i].addEventListener('click', changeLayer);
+};
+
+// event listener for number string
+for (let i = 0; i < myBoxes.length; i++) {
+  myBoxesLayerB[i].addEventListener('click', loopThroughNumber);
 };
