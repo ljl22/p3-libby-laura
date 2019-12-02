f28cd920af124e03ecb0ab4d8b22fe6899e9e492
ljl22 <lauraj.libby@gmail.com>
2019-12-02 17:58:02 -0500

    Update JS to make all three functions and fortune randomizer work

--- src/main.js
+++ src/main.js
@@ -8,12 +8,13 @@ const layerB = document.getElementById('step-b');
 
 const myBoxesLayerC = document.getElementsByClassName('blockc');
 const layerC = document.getElementById('step-c');
+
 //fortunes start here
 const irishFortunes = ['A drink precedes a story', 'Youth does not mind where it sets its foot', 'You’ve got to do your own growing, no matter how tall your father was', 'The well-fed does not understand the lean', 'A good laugh is the  best cure', 'Do not take the thatch from your own roof to buy slates for another man’s house', 'There’s no use boiling your cabbage twice', 'A tune is more lasting than the song of the birds & a word more lasting than the wealth of the world', 'It’s as easy to catch a cold in a King’s castle as in a sheppard’s hut', 'Lose an hour in the morning and you’ll be looking for it all day'];
 const fortuneResults = document.getElementById('fortune-results');
 
-
-// our function - local scope
+// LAYER 1
+// our function - local scope - loop through word
 function loopThrough() {
   const changeColor = () => {
     this.classList.toggle('lime');
@@ -32,15 +33,12 @@ function changeLayer () {
  setTimeout(swap, this.textContent.length * 2 * 1000);
 };
 
-//function for layer THREE reveal***********
-function changeLayerThree() {
-  const swap = () => {
-    layerB.classList.add('hide');
-    layerC.classList.remove('hide2');
-  }
- setTimeout(swap, this.textContent.length * 2 * 1000);
+for (let i = 0; i < myBoxes.length; i++) {
+  myBoxes[i].addEventListener('click', loopThrough);
+  myBoxes[i].addEventListener('click', changeLayer);
 };
 
+// LAYER Two
 //loop through for number string************************
 function loopThroughNumber() {
   const changeColorTwo = () => this.classList.toggle('forest')
@@ -49,42 +47,39 @@ function loopThroughNumber() {
     setTimeout(changeColorTwo, i * 1000);
   }
 }
-loopThroughNumber();
 
-//function for random fortunes*****************should this be within a function?
-const randomizeFortunes = () => {
-  irishFortunes.sort((a, b) => {return 0.5 - Math.random()});
-  const fortuneTextNode = document.createTextNode(irishFortunes[0]);
-  const fortuneParagraph = document.createElement('p');
-  fortuneParagraph.appendChild(fortuneTextNode);
-  fortuneResults.appendChild(fortuneParagraph);
-}
-
-//function randomResults(){
-//  const randomizeFortunes = () => {
-//    irishFortunes.sort((a, b) => {return 0.5 - Math.random()});
-//    const fortuneTextNode = document.createTextNode(irishFortunes[0]);
-//    const fortuneParagraph = document.createElement('p');
-//    fortuneParagraph.appendChild(fortuneTextNode);
-//    fortuneResults.appendChild(fortuneParagraph);
-//  }
-//}
-// randomResults()
-myBoxesLayerC.addEventListener('click', randomizeFortunes); // I know this needs to most likely live elsewhere and  not be tied to length of number
-
-
-// event listener for box change to number
-for (let i = 0; i < myBoxes.length; i++) {
-  myBoxes[i].addEventListener('click', loopThrough);
-  myBoxes[i].addEventListener('click', changeLayer);
+//function for layer THREE reveal***********
+function changeLayerTwo() {
+  const swap = () => {
+    layerB.classList.add('hide');
+    layerC.classList.remove('hide2');
+  }
+ setTimeout(swap,  Number(this.textContent) * 2 * 1000);
 };
 
 // event listener for number string
 for (let i = 0; i < myBoxes.length; i++) {
   myBoxesLayerB[i].addEventListener('click', loopThroughNumber);
+  myBoxesLayerB[i].addEventListener('click', changeLayerTwo);
 };
 
+// LAYER THREE
 //for second number transition
+//function for layer THREE reveal***********
+function changeLayerThree() {
+  const swap = () => {
+    layerC.classList.add('hide');
+    fortuneResults.classList.remove('hide2');
+    irishFortunes.sort((a, b) => {return 0.5 - Math.random()});
+    const fortuneTextNode = document.createTextNode(irishFortunes[0]);
+    const fortuneParagraph = document.createElement('p');
+    fortuneParagraph.appendChild(fortuneTextNode);
+    fortuneResults.appendChild(fortuneParagraph);
+  }
+  setTimeout(swap,  Number(this.textContent) * 2 * 1000);
+};
+
 for (let i = 0; i < myBoxes.length; i++) {
- myBoxesLayerC[i].addEventListener('click', changeLayerThree);
+  myBoxesLayerC[i].addEventListener('click', loopThroughNumber);
+  myBoxesLayerC[i].addEventListener('click', changeLayerThree);
 };
